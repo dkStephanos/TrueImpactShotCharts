752e79b9bfcfb24c65430edc64e59d0cbe2980fe
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-04-11 19:33:06 -0400

    load game method

--- code/EventProcessor.py
+++ code/EventProcessor.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 class EventProcessor:
+    def load_game(game_id):
+        event_df = pd.load_csv('/data/src/events.csv')
+        return event_df.loc[event_df['GameId'] == game_id]
+    
     def extract_shots(event_df):
         # Initialize an empty list to hold the indices of offensive rebounds
         return event_df.loc[event_df['eventType'] == 'SHOT']
