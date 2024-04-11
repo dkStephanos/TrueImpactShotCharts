d6083964df2d40775d0d96fa84a8826155076cfb
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-04-11 19:33:14 -0400

    init

---
+++ code/TrackingProcessor.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+class TrackingProcessor:
+    def load_game(game_id):
+        tracking_df = pd.load_csv('/data/src/tracking.csv')
+        return tracking_df.loc[tracking_df['GameId'] == game_id]
+    
+    def extract_player_locations_at_timestamp(tracking_df, timestamp):
+        return tracking_df.loc[tracking_df['WcTime'] == timestamp]
