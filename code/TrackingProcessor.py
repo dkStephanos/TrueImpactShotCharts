6d0ba9a577ecd67783c76566968c622541364e78
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-04-12 21:36:26 -0400

    adding extract_possession_moments

--- code/TrackingProcessor.py
+++ code/TrackingProcessor.py
@@ -2,8 +2,12 @@ import pandas as pd
 
 class TrackingProcessor:
     def load_game(game_id):
-        tracking_df = pd.read_csv('data/src/tracking.csv', dtype={'gameId': str})
+        tracking_df = pd.read_csv('data/src/tracking.csv', dtype={'gameId': str, 'playerId': str})
         return tracking_df.loc[tracking_df['gameId'] == game_id]
     
     def extract_player_locations_at_timestamp(tracking_df, timestamp):
         return tracking_df.loc[tracking_df['wcTime'] == timestamp]
+    
+    def extract_possession_moments(tracking_df, possession):
+        """Extract moments for the specified time frame from the game DataFrame."""
+        return tracking_df.loc[(tracking_df["wcTime"] >= possession["wcStart"]) & (tracking_df["wcTime"] <= possession["wcEnd"])]
