f64bf2d4a309f6bf78f1b02cbe4a986b035bb832
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-04-20 14:25:44 -0400

    adding is_position_in_paint method
    moving the other methods as they make more sense with the io stuff as they primarily are just extracting data, not creating new data points as this does

--- code/util/FeatureUtil.py
+++ code/util/FeatureUtil.py
@@ -1,26 +1,26 @@
 import pandas as pd
-
 class FeatureUtil:
-    def player_positions_at_moment(moment_df, timestamp, player_ids="all"):
-        # Handle the player_ids input
-        if isinstance(player_ids, list):
-            pass  # If player_ids is already a list, do nothing
-        elif player_ids == "all":
-            player_ids = list(moment_df["playerId"].dropna().unique())  # Get all unique playerIds if 'all'
+    def is_position_in_paint(x, y):
+        """
+        Determine if a position (x, y) is in the paint of an NBA basketball court centered at (0, 0).
+        The paint is 16 feet wide and extends from the baseline up to 19 feet towards the center.
         
-        # Filter the DataFrame for the specified timestamp and player_ids
-        moment_df = moment_df[(moment_df["timestamp"] == timestamp) & (moment_df["playerId"].isin(player_ids))]
-        
-        # Drop unnecessary columns
-        moment_df = moment_df.drop(["gameId", "teamAbbr", "period", "wcTime", "gcTime", "scTime", "gameDate"], axis=1)
-        
-        return moment_df
+        Args:
+        x (float): The x-coordinate of the position (horizontal distance from the centerline in feet).
+        y (float): The y-coordinate of the position (vertical distance from the centerline in feet).
 
-    def ball_position_at_moment(moment_df, timestamp):
-        # Filter the DataFrame for the specified timestamp and the ball (team_id = -1)
-        ball_df = moment_df[(moment_df["timestamp"] == timestamp) & (moment_df["teamId"] == -1)]
-        
-        # Drop unnecessary columns
-        ball_df = ball_df.drop(["gameId", "teamAbbr", "period", "wcTime", "gcTime", "scTime", "gameDate"], axis=1)
-        
-        return ball_df
+        Returns:
+        bool: True if the position is in the paint, otherwise False.
+        """
+        # Dimensions of the paint area
+        PAINT_WIDTH_HALF = 8  # Half the width of the paint (16 feet total)
+        PAINT_LENGTH = 19  # Length of the paint from the baseline towards the center
+
+        # Adjust for the paint being vertically centered on the y-axis
+        # The paint extends from the baseline to 19 feet towards the center
+        in_vertical_bounds = abs(y) <= PAINT_LENGTH
+
+        # The paint is horizontally centered around the basket (which is centered at x=0)
+        in_horizontal_bounds = -PAINT_WIDTH_HALF <= x <= PAINT_WIDTH_HALF
+
+        return in_vertical_bounds and in_horizontal_bounds
