9806a1ab6e2ac4f943824a662fbe2e43d47317d6
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-04-20 14:18:14 -0400

    reorg of methods

--- code/io/TrackingProcessor.py
+++ code/io/TrackingProcessor.py
@@ -21,8 +21,29 @@ class TrackingProcessor:
         tracking_df = pd.read_csv('data/src/tracking.csv', dtype={'gameId': str, 'playerId': str})
         return tracking_df.loc[tracking_df['gameId'] == game_id]
     
-    def extract_player_locations_at_timestamp(tracking_df, timestamp):
-        return tracking_df.loc[tracking_df['wcTime'] == timestamp]
+    def player_positions_at_moment(moment_df, timestamp, player_ids="all"):
+        # Handle the player_ids input
+        if isinstance(player_ids, list):
+            pass  # If player_ids is already a list, do nothing
+        elif player_ids == "all":
+            player_ids = list(moment_df["playerId"].dropna().unique())  # Get all unique playerIds if 'all'
+        
+        # Filter the DataFrame for the specified timestamp and player_ids
+        moment_df = moment_df[(moment_df["timestamp"] == timestamp) & (moment_df["playerId"].isin(player_ids))]
+        
+        # Drop unnecessary columns
+        moment_df = moment_df.drop(["gameId", "teamAbbr", "period", "wcTime", "gcTime", "scTime", "gameDate"], axis=1)
+        
+        return moment_df
+
+    def ball_position_at_moment(moment_df, timestamp):
+        # Filter the DataFrame for the specified timestamp and the ball (team_id = -1)
+        ball_df = moment_df[(moment_df["timestamp"] == timestamp) & (moment_df["teamId"] == -1)]
+        
+        # Drop unnecessary columns
+        ball_df = ball_df.drop(["gameId", "teamAbbr", "period", "wcTime", "gcTime", "scTime", "gameDate"], axis=1)
+        
+        return ball_df
     
     def extract_possession_moments(tracking_df, possession):
         """Extract moments for the specified time frame (as defined by the incoming possession dict) from the game DataFrame."""
