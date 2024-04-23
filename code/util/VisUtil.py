872cc5056618b201a1bfe774e0fe43b6cafa740b
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-04-23 00:20:22 -0400

    refactor, updates, cleanup

--- code/util/VisUtil.py
+++ code/util/VisUtil.py
@@ -8,6 +8,7 @@ from typing import List, Dict, Tuple
 from code.io.TrackingProcessor import TrackingProcessor
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
+
 class VisUtil:
     INTERVAL: int = 2
     COURT_DIMS: Tuple[int, int, int, int] = (
@@ -66,12 +67,27 @@ class VisUtil:
         team_ids = tracking_df["teamId"].head(25).dropna().unique()
         self.home_team_id = team_ids[0]
         self.away_team_id = team_ids[1]
-        self.home_team_tuple = (self.TEAM_COLOR_DICT[team_ids[0]][1], self.TEAM_COLOR_DICT[team_ids[0]][0])
-        self.away_team_tuple = (self.TEAM_COLOR_DICT[team_ids[1]][1], self.TEAM_COLOR_DICT[team_ids[1]][0])
+        self.home_team_tuple = (
+            self.TEAM_COLOR_DICT[team_ids[0]][1],
+            self.TEAM_COLOR_DICT[team_ids[0]][0],
+        )
+        self.away_team_tuple = (
+            self.TEAM_COLOR_DICT[team_ids[1]][1],
+            self.TEAM_COLOR_DICT[team_ids[1]][0],
+        )
 
-        players_df = pd.read_csv("data/src/basic_player_info.csv", dtype={'person_id': str, 'jersey_num': str})
-        self.players_dict = {row['person_id']: (f"{row['nickname'][0]}. {row['last_name']}", row['jersey_num']) for index, row in players_df.iterrows()}
-        
+        players_df = pd.read_csv(
+            "data/src/basic_player_info.csv",
+            dtype={"person_id": str, "jersey_num": str},
+        )
+        self.players_dict = {
+            row["person_id"]: (
+                f"{row['nickname'][0]}. {row['last_name']}",
+                row["jersey_num"],
+            )
+            for index, row in players_df.iterrows()
+        }
+
         self.fig, self.ax = None, None
         self.last_animation = (
             {}
@@ -80,7 +96,7 @@ class VisUtil:
 
     def setup_court(self, moments_df):
         """Configure the matplotlib figure and axes for the animation."""
-        
+
         x_min, x_max, y_min, y_max = self.COURT_DIMS
         self.ax.set_xlim(x_min, x_max)
         self.ax.set_ylim(y_min, y_max)
@@ -101,21 +117,35 @@ class VisUtil:
         cell_colours = [column_colours for _ in range(5)]
 
         # Collect home and away player_ids (ball has a teamId of -1)
-        players_df = moments_df.loc[moments_df['teamId'] != -1, ['playerId', 'playerName', 'teamId']].drop_duplicates()
-        home_players_ids = list(players_df.loc[players_df["teamId"] == self.home_team_id]["playerId"].unique())
-        away_players_ids = list(players_df.loc[players_df["teamId"] == self.away_team_id]["playerId"].unique())
+        players_df = moments_df.loc[
+            moments_df["teamId"] != -1, ["playerId", "playerName", "teamId"]
+        ].drop_duplicates()
+        home_players_ids = list(
+            players_df.loc[players_df["teamId"] == self.home_team_id][
+                "playerId"
+            ].unique()
+        )
+        away_players_ids = list(
+            players_df.loc[players_df["teamId"] == self.away_team_id][
+                "playerId"
+            ].unique()
+        )
         all_players_ids = home_players_ids + away_players_ids
 
         # Create player cell entries
         home_players = [
-            " #".join([self.players_dict[player_id][0], str(self.players_dict[player_id][1])])
+            " #".join(
+                [self.players_dict[player_id][0], str(self.players_dict[player_id][1])]
+            )
             for player_id in home_players_ids
         ]
         away_players = [
-            " #".join([self.players_dict[player_id][0], str(self.players_dict[player_id][1])])
+            " #".join(
+                [self.players_dict[player_id][0], str(self.players_dict[player_id][1])]
+            )
             for player_id in away_players_ids
         ]
-        
+
         # Render and scale the table
         table = plt.table(
             cellText=list(zip(home_players, away_players)),
@@ -137,34 +167,45 @@ class VisUtil:
             horizontalalignment="center",
             verticalalignment="center",
         )
-        
+
         # Set up annotations for players dynamically based on current player positions in the frame
         annotations = {
             player_id: self.ax.annotate(
                 f"{self.players_dict[player_id][1]}",  # jersey_num
-                xy=(0, 0),  # Initial position; this should be updated dynamically in the animation loop
+                xy=(
+                    0,
+                    0,
+                ),  # Initial position; this should be updated dynamically in the animation loop
                 color="w",
                 horizontalalignment="center",
                 verticalalignment="center",
-                fontweight="bold"
+                fontweight="bold",
             )
             for player_id in all_players_ids
         }
-        
+
         # Set up and add circles for the players and ball
         self.player_circles = {
             player_id: Circle(
                 (0, 0),  # Initial position; this should be updated dynamically
                 self.PLAYER_CIRCLE_SIZE,
-                color=self.TEAM_COLOR_DICT[self.home_team_id if player_id in home_players_ids else self.away_team_id][0]  # Team color based on player's team
+                color=self.TEAM_COLOR_DICT[
+                    (
+                        self.home_team_id
+                        if player_id in home_players_ids
+                        else self.away_team_id
+                    )
+                ][
+                    0
+                ],  # Team color based on player's team
             )
             for player_id in all_players_ids
         }
-        
+
         self.ball_circle = Circle(
             (0, 0),  # Initial ball position
             self.PLAYER_CIRCLE_SIZE,
-            color="#ff8c00"  # Hardcoded orange for the ball
+            color="#ff8c00",  # Hardcoded orange for the ball
         )
 
         # Add all player circles and the ball circle to the axes
@@ -175,37 +216,45 @@ class VisUtil:
         return annotations, clock_info
 
     def precompute_frames(moments_df):
-         # First, add a frame_id to group on
+        # First, add a frame_id to group on
         moments_df = moments_df.copy()
-        moments_df.loc[:, 'frame_id'] = moments_df['wcTime'].factorize()[0]
-        
+        moments_df.loc[:, "frame_id"] = moments_df["wcTime"].factorize()[0]
+
         # Include ball data and player data without filtering out the ball prematurely
-        player_frames = moments_df.drop(columns=['gameId', 'teamAbbr', 'gameDate'])  # Keep ball data
+        player_frames = moments_df.drop(
+            columns=["gameId", "teamAbbr", "gameDate"]
+        )  # Keep ball data
 
         # Precompute player data for each frame, ensure to separate ball data here
-        player_data_by_frame = player_frames[player_frames['teamId'] != "-1"].groupby('frame_id').apply(
-            lambda df: df[['playerId', 'x', 'y', 'z']].to_dict(orient='records')
+        player_data_by_frame = (
+            player_frames[player_frames["teamId"] != "-1"]
+            .groupby("frame_id")
+            .apply(lambda df: df[["playerId", "x", "y", "z"]].to_dict(orient="records"))
         )
 
         # Separately handle ball data to ensure it's correctly processed
-        ball_data_by_frame = player_frames[player_frames['teamId'] == "-1"].groupby('frame_id').apply(
-            lambda df: df[['x', 'y', 'z']].iloc[0].to_dict()
+        ball_data_by_frame = (
+            player_frames[player_frames["teamId"] == "-1"]
+            .groupby("frame_id")
+            .apply(lambda df: df[["x", "y", "z"]].iloc[0].to_dict())
         )
 
         # Now create the precomputed_data list
         return [
             {
-                'quarter': frame['period'].iloc[0],  # Assuming period is the same for all rows in the same frame
-                'game_clock': frame['gcTime'].iloc[0],
-                'shot_clock': frame['scTime'].iloc[0],
-                'ball': {
-                    'x': ball_data_by_frame.loc[idx]['x'],
-                    'y': ball_data_by_frame.loc[idx]['y'],
-                    'z': ball_data_by_frame.loc[idx]['z']
+                "quarter": frame["period"].iloc[
+                    0
+                ],  # Assuming period is the same for all rows in the same frame
+                "game_clock": frame["gcTime"].iloc[0],
+                "shot_clock": frame["scTime"].iloc[0],
+                "ball": {
+                    "x": ball_data_by_frame.loc[idx]["x"],
+                    "y": ball_data_by_frame.loc[idx]["y"],
+                    "z": ball_data_by_frame.loc[idx]["z"],
                 },
-                'players': player_data_by_frame.loc[idx]
+                "players": player_data_by_frame.loc[idx],
             }
-            for idx, frame in player_frames.groupby('frame_id')
+            for idx, frame in player_frames.groupby("frame_id")
         ]
 
     def animate(self, frame: int, precomputed_data, annotations, clock_info):
@@ -219,23 +268,29 @@ class VisUtil:
         # Update player positions and annotations using player IDs as dictionary keys
         updated_artists = [self.ball_circle, clock_info]
 
-        for player in moment['players']:
-            player_id = player['playerId']  # Assuming each player dictionary has an 'id' key
+        for player in moment["players"]:
+            player_id = player[
+                "playerId"
+            ]  # Assuming each player dictionary has an 'id' key
             if player_id in annotations:
-                self.player_circles[player_id].center = (player['x'], player['y'])
-                annotations[player_id].set_position((player['x'], player['y']))
-                updated_artists.extend([self.player_circles[player_id], annotations[player_id]])
+                self.player_circles[player_id].center = (player["x"], player["y"])
+                annotations[player_id].set_position((player["x"], player["y"]))
+                updated_artists.extend(
+                    [self.player_circles[player_id], annotations[player_id]]
+                )
 
         # Update the ball's position and radius
-        self.ball_circle.center = (moment['ball']['x'], moment['ball']['y'])
-        self.ball_circle.radius = moment['ball']['z'] / self.NORMALIZATION_COEF
-        
+        self.ball_circle.center = (moment["ball"]["x"], moment["ball"]["y"])
+        self.ball_circle.radius = moment["ball"]["z"] / self.NORMALIZATION_COEF
+
         return updated_artists
 
     def _create_animation(self, possession, interval: int):
         """Support method for display/save methods with caching support."""
         self.fig, self.ax = plt.subplots(figsize=(12, 8))
-        moments_df = TrackingProcessor.extract_possession_moments(self.tracking_df, possession)
+        moments_df = TrackingProcessor.extract_possession_moments(
+            self.tracking_df, possession
+        )
         annotations, clock_info = self.setup_court(moments_df)
         precomputed_data = self.precompute_frames(moments_df)
 
@@ -248,35 +303,35 @@ class VisUtil:
             blit=True,
         )
 
-    def display_animation(self, possession):        
+    def display_animation(self, possession):
         """Configure and display the animation for the specified possession number. Designed for inline Jupyter cells"""
-        anim = self._create_animation(
-            possession=possession, interval=40
-        )
+        anim = self._create_animation(possession=possession, interval=40)
         return HTML(anim.to_html5_video())
 
-    def save_animation(self, possession, filename='animation.mp4'):
+    def save_animation(self, possession, filename="animation.mp4"):
         """Save the animation for the specified possession number to a file."""
         anim = self._create_animation(
             possession=possession, interval=40
         )  # Create the animation
 
         # Check the filename extension to decide on writer
-        if filename.endswith('.gif'):
+        if filename.endswith(".gif"):
             writer = animation.PillowWriter(fps=25)
         else:
             # This requires having ffmpeg installed
-            writer = 'ffmpeg'  # Default writer for MP4 and AVI
-        
+            writer = "ffmpeg"  # Default writer for MP4 and AVI
+
         # Save the animation
-        anim.save(filename, writer=writer, dpi=80)  # You can adjust the DPI based on your resolution needs
+        anim.save(
+            filename, writer=writer, dpi=80
+        )  # You can adjust the DPI based on your resolution needs
 
     def plot_frame_at_timestamp(self, timestamp):
         """Plot a single frame based on the given timestamp."""
         self.fig, self.ax = plt.subplots(figsize=(12, 8))
-        
+
         # Filter the dataframe for the specific timestamp
-        moments_df = self.tracking_df[self.tracking_df['wcTime'] == timestamp]
+        moments_df = self.tracking_df[self.tracking_df["wcTime"] == timestamp]
 
         if moments_df.empty:
             raise ValueError("No data available for the specified timestamp")
@@ -286,21 +341,21 @@ class VisUtil:
 
         # Since we only need one frame, we can directly take the necessary data
         moment = moments_df.iloc[0]
-        game_clock = moment['gcTime']
-        shot_clock = moment['scTime']
-        quarter = moment['period']
+        game_clock = moment["gcTime"]
+        shot_clock = moment["scTime"]
+        quarter = moment["period"]
 
         # Update the annotations and player positions
         for index, row in moments_df.iterrows():
-            player_id = row['playerId']
+            player_id = row["playerId"]
             if player_id in self.player_circles:
-                self.player_circles[player_id].center = (row['x'], row['y'])
-                annotations[player_id].set_position((row['x'], row['y']))
+                self.player_circles[player_id].center = (row["x"], row["y"])
+                annotations[player_id].set_position((row["x"], row["y"]))
 
         # Update the ball's position
-        ball_data = moments_df[moments_df['teamId'] == "-1"].iloc[0]
-        self.ball_circle.center = (ball_data['x'], ball_data['y'])
-        self.ball_circle.radius = ball_data['z'] / self.NORMALIZATION_COEF
+        ball_data = moments_df[moments_df["teamId"] == "-1"].iloc[0]
+        self.ball_circle.center = (ball_data["x"], ball_data["y"])
+        self.ball_circle.radius = ball_data["z"] / self.NORMALIZATION_COEF
 
         # Update the clock info
         clock_info.set_text(
@@ -310,44 +365,86 @@ class VisUtil:
         # Redraw the frame with the updated positions and information
         self.ax.figure.canvas.draw()
         plt.show()
-        
+
     def plot_voronoi_at_timestamp(self, timestamp):
-        """Plot the Voronoi diagram for the players' positions at a specific timestamp."""
+        """Plot the Voronoi diagram for the players' positions at a specific timestamp with team-based cell shading."""
         self.fig, self.ax = plt.subplots(figsize=(12, 8))
-        moments_df = self.tracking_df[self.tracking_df['wcTime'] == timestamp]
-        
+        moments_df = self.tracking_df[self.tracking_df["wcTime"] == timestamp]
+
         if moments_df.empty:
             raise ValueError("No data available for the specified timestamp")
-        
-        annotations, clock_info = self.setup_court(moments_df)  # Set up the court and game elements
-        
-        # Extract player positions and filter out the ball
-        player_positions = moments_df[moments_df['teamId'] != "-1"][['x', 'y']].values
+
+        annotations, clock_info = self.setup_court(
+            moments_df
+        )  # Set up the court and game elements
+
+        # Extract player positions and team IDs, and filter out the ball
+        player_data = moments_df[moments_df["teamId"] != "-1"][
+            ["x", "y", "teamId"]
+        ].values
+
+        # Extract ball data
+        ball_data = (
+            moments_df[moments_df["teamId"] == "-1"].iloc[0]
+            if not moments_df[moments_df["teamId"] == "-1"].empty
+            else None
+        )
 
         # Compute the Voronoi diagram
-        vor = Voronoi(player_positions)
+        vor = Voronoi(player_data[:, :2])
 
-        # Plot the Voronoi diagram
-        voronoi_plot_2d(vor, ax=self.ax, show_vertices=False, show_points=False, line_colors='orange', line_width=2, line_alpha=0.6)
+        # Plot the Voronoi diagram, ensuring vertices are not shown
+        voronoi_plot_2d(
+            vor,
+            ax=self.ax,
+            show_vertices=False,
+            show_points=False,
+            line_colors="orange",
+            line_width=2,
+            line_alpha=0.6,
+        )
+
+        # Color each Voronoi region based on the player's team color
+        for region, index in zip(vor.regions, vor.point_region):
+            vertices = vor.vertices[region]
+            if all(v >= 0 for v in region):  # Check if the region is bounded
+                team_id = player_data[index][2]
+                team_color = self.TEAM_COLOR_DICT.get(team_id)[
+                    0
+                ]  # Default to gray if team color not found
+                polygon = plt.Polygon(
+                    vertices, color=team_color, alpha=0.3
+                )  # Set alpha for some transparency
+                self.ax.add_patch(polygon)
 
         # Overlay player circles and annotations on top of the Voronoi diagram for clarity
         for index, row in moments_df.iterrows():
-            player_id = row['playerId']
+            player_id = row["playerId"]
             if player_id in self.player_circles:
                 circle = self.player_circles[player_id]
-                circle.center = (row['x'], row['y'])
+                circle.center = (row["x"], row["y"])
                 self.ax.add_patch(circle)  # Make sure the circle is added to the plot
 
                 # Add annotations directly
                 self.ax.annotate(
                     f"{self.players_dict[player_id][1]}",  # jersey_num
-                    xy=(row['x'], row['y']),
+                    xy=(row["x"], row["y"]),
                     color="white",
                     ha="center",  # horizontal alignment
                     va="center",  # vertical alignment
                     fontweight="bold",
-                    fontsize=10
+                    fontsize=10,
                 )
+
+        # Plot the ball
+        if ball_data is not None:
+            ball_circle = Circle(
+                (ball_data["x"], ball_data["y"]),
+                ball_data["z"] / self.NORMALIZATION_COEF,
+                color="orange",
+                zorder=5,
+            )
+            self.ax.add_patch(ball_circle)
 
         # Redraw the frame with the updated positions and Voronoi diagram overlay
         self.ax.figure.canvas.draw()
