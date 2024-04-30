a0f980cd08a3dc6d6276fb33b8b4b7cbe3086ff8
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-04-29 22:10:14 -0400

    small progress on regions

--- code/util/ShotRegionUtil.py
+++ code/util/ShotRegionUtil.py
@@ -7,104 +7,93 @@ THREE_POINT_RADIUS = 23.75
 THREE_POINT_SIDELINE_DISTANCE = 14
 CORNER_THREE_DISTANCE = 22
 RESTRICTED_AREA_RADIUS = 4
+COURT_LENGTH = 94
 COURT_WIDTH_HALF = 25
 FREE_THROW_LINE_DISTANCE = 19
 ELBOW_DISTANCE_FROM_CENTER = 19
+X_MIN = 0
+X_MAX = 47
+Y_MIN = -25
+Y_MAX = 25
 
 # Generate the three-point arc segments
-ARC_START_ANGLE = -70  # Start angle for the three-point arc
-ARC_END_ANGLE = 70  # End angle for the three-point arc
 arc = [
     Point(
         BASKET_X + THREE_POINT_RADIUS * np.cos(np.radians(angle)),
-        THREE_POINT_RADIUS * np.sin(np.radians(angle)),
+        THREE_POINT_RADIUS * np.sin(np.radians(angle))
     )
-    for angle in np.linspace(ARC_START_ANGLE, ARC_END_ANGLE, num=100)
+    for angle in np.linspace(-70, 70, num=100)
 ]
-left_arc = [
-    Point(p.x - 2 * BASKET_X, p.y) for p in arc
-]  # Mirror the arc to the left side
 
-basket_center = Point(BASKET_X, 0)
-restricted_area_circle = basket_center.buffer(RESTRICTED_AREA_RADIUS)
+# Generate the three-point arc for the right half-court
+right_arc = [p for p in arc if p.x >= BASKET_X]
+
+# Create the restricted area polygon
+restricted_area = Point(BASKET_X, 0).buffer(RESTRICTED_AREA_RADIUS).difference(Point(BASKET_X, 0).buffer(RESTRICTED_AREA_RADIUS).boundary)
+
 class ShotRegionUtil:
     regions = {
-        'RESTRICTED_AREA': Polygon(restricted_area_circle.exterior.coords),
-        'LEFT_CORNER_THREE': Polygon([
-            (-BASKET_X, -COURT_WIDTH_HALF),
-            (-BASKET_X, CORNER_THREE_DISTANCE - COURT_WIDTH_HALF),
-            tuple(left_arc[0].coords[0]),
-            tuple(left_arc[-1].coords[0]),
-            (-BASKET_X + THREE_POINT_SIDELINE_DISTANCE, CORNER_THREE_DISTANCE - COURT_WIDTH_HALF),
-            (-BASKET_X + THREE_POINT_SIDELINE_DISTANCE, -COURT_WIDTH_HALF),
-        ]),
+        'RESTRICTED_AREA': restricted_area,
         'RIGHT_CORNER_THREE': Polygon([
-            (BASKET_X, -COURT_WIDTH_HALF),
-            (BASKET_X, CORNER_THREE_DISTANCE - COURT_WIDTH_HALF),
-            tuple(arc[0].coords[0]),
-            tuple(arc[-1].coords[0]),
-            (BASKET_X - THREE_POINT_SIDELINE_DISTANCE, CORNER_THREE_DISTANCE - COURT_WIDTH_HALF),
-            (BASKET_X - THREE_POINT_SIDELINE_DISTANCE, -COURT_WIDTH_HALF),
+            (BASKET_X, Y_MIN),
+            (X_MAX, Y_MIN),
+            (X_MAX, -THREE_POINT_SIDELINE_DISTANCE),
+            tuple(right_arc[0].coords[0]),
         ]),
-        'LEFT_WING_THREE': Polygon(
-        [
-            (BASKET_X - THREE_POINT_SIDELINE_DISTANCE, -FREE_THROW_LINE_DISTANCE),
-            (BASKET_X - THREE_POINT_SIDELINE_DISTANCE, -COURT_WIDTH_HALF),
-            tuple(left_arc[-1].coords[0]),
-            tuple(left_arc[0].coords[0]),
-        ]
-    ),
-    'RIGHT_WING_THREE': Polygon(
-        [
-            (BASKET_X - THREE_POINT_SIDELINE_DISTANCE, -FREE_THROW_LINE_DISTANCE),
-            (BASKET_X - THREE_POINT_SIDELINE_DISTANCE, -COURT_WIDTH_HALF),
-            tuple(arc[-1].coords[0]),
-            tuple(arc[0].coords[0]),
-        ]
-    ),
-    'LEFT_BASELINE_MID': Polygon(
-        [
-            (-BASKET_X, -COURT_WIDTH_HALF),
-            (-BASKET_X, -FREE_THROW_LINE_DISTANCE),
-            (-BASKET_X + THREE_POINT_SIDELINE_DISTANCE, -FREE_THROW_LINE_DISTANCE),
-            (-BASKET_X + THREE_POINT_SIDELINE_DISTANCE, -COURT_WIDTH_HALF),
-        ]
-    ),
-    'RIGHT_BASELINE_MID': Polygon(
-        [
-            (BASKET_X, -COURT_WIDTH_HALF),
-            (BASKET_X, -FREE_THROW_LINE_DISTANCE),
-            (BASKET_X - THREE_POINT_SIDELINE_DISTANCE, -FREE_THROW_LINE_DISTANCE),
-            (BASKET_X - THREE_POINT_SIDELINE_DISTANCE, -COURT_WIDTH_HALF),
-        ]
-    ),
-    'LEFT_ELBOW_MID': Polygon(
-        [
-            (-BASKET_X + THREE_POINT_SIDELINE_DISTANCE, -FREE_THROW_LINE_DISTANCE),
-            (-BASKET_X + THREE_POINT_SIDELINE_DISTANCE, -ELBOW_DISTANCE_FROM_CENTER),
-            (-BASKET_X, -ELBOW_DISTANCE_FROM_CENTER),
-            (-BASKET_X, -FREE_THROW_LINE_DISTANCE),
-        ]
-    ),
-    'RIGHT_ELBOW_MID': Polygon(
-        [
-            (BASKET_X - THREE_POINT_SIDELINE_DISTANCE, -FREE_THROW_LINE_DISTANCE),
-            (BASKET_X - THREE_POINT_SIDELINE_DISTANCE, -ELBOW_DISTANCE_FROM_CENTER),
+        'RIGHT_WING_THREE': Polygon([
+            tuple(right_arc[0].coords[0]),
+            tuple(right_arc[-1].coords[0]),
+            (BASKET_X, -THREE_POINT_SIDELINE_DISTANCE),
+        ]),
+        'RIGHT_BASELINE_MID': Polygon([
+            (BASKET_X, -THREE_POINT_SIDELINE_DISTANCE),
+            (BASKET_X, Y_MIN),
+            (BASKET_X - FREE_THROW_LINE_DISTANCE, Y_MIN),
+            (BASKET_X - FREE_THROW_LINE_DISTANCE, -THREE_POINT_SIDELINE_DISTANCE),
+        ]),
+        'RIGHT_ELBOW_MID': Polygon([
+            (BASKET_X, -THREE_POINT_SIDELINE_DISTANCE),
+            (BASKET_X - FREE_THROW_LINE_DISTANCE, -THREE_POINT_SIDELINE_DISTANCE),
+            (BASKET_X - FREE_THROW_LINE_DISTANCE, -ELBOW_DISTANCE_FROM_CENTER),
             (BASKET_X, -ELBOW_DISTANCE_FROM_CENTER),
-            (BASKET_X, -FREE_THROW_LINE_DISTANCE),
-        ]
-    ),
-    'CENTER_THREE': Polygon(arc),
-    'BEYOND_HALFCOURT': Polygon(
-        [
-            (-BASKET_X, -COURT_WIDTH_HALF),
-            (-BASKET_X, COURT_WIDTH_HALF),
-            (BASKET_X, COURT_WIDTH_HALF),
-            (BASKET_X, -COURT_WIDTH_HALF),
-        ]
-    )
+        ]),
+        'LEFT_CORNER_THREE': Polygon([
+            (BASKET_X, THREE_POINT_SIDELINE_DISTANCE),
+            (X_MAX, THREE_POINT_SIDELINE_DISTANCE),
+            (X_MAX, Y_MAX),
+            (BASKET_X, Y_MAX),
+        ]),
+        'LEFT_WING_THREE': Polygon([
+            tuple(right_arc[-1].coords[0]),
+            (BASKET_X, THREE_POINT_SIDELINE_DISTANCE),
+            (BASKET_X, Y_MAX),
+        ]),
+        'LEFT_BASELINE_MID': Polygon([
+            (BASKET_X, Y_MAX),
+            (BASKET_X - FREE_THROW_LINE_DISTANCE, Y_MAX),
+            (BASKET_X - FREE_THROW_LINE_DISTANCE, THREE_POINT_SIDELINE_DISTANCE),
+            (BASKET_X, THREE_POINT_SIDELINE_DISTANCE),
+        ]),
+        'LEFT_ELBOW_MID': Polygon([
+            (BASKET_X, ELBOW_DISTANCE_FROM_CENTER),
+            (BASKET_X - FREE_THROW_LINE_DISTANCE, ELBOW_DISTANCE_FROM_CENTER),
+            (BASKET_X - FREE_THROW_LINE_DISTANCE, THREE_POINT_SIDELINE_DISTANCE),
+            (BASKET_X, THREE_POINT_SIDELINE_DISTANCE),
+        ]),
+        'CENTER_THREE': Polygon([
+            tuple(right_arc[-1].coords[0]),
+            (BASKET_X - FREE_THROW_LINE_DISTANCE, THREE_POINT_SIDELINE_DISTANCE),
+            (BASKET_X - FREE_THROW_LINE_DISTANCE, -THREE_POINT_SIDELINE_DISTANCE),
+            tuple(right_arc[0].coords[0]),
+        ]),
+        'BEYOND_HALFCOURT': Polygon([
+            (X_MIN, Y_MIN),
+            (X_MIN, Y_MAX),
+            (BASKET_X, Y_MAX),
+            (BASKET_X, Y_MIN),
+        ]),
     }
-    
+
     region_colors = {
         'RESTRICTED_AREA': '#FF0000',  # Red
         'LEFT_CORNER_THREE': '#00FF00',  # Green
