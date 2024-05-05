e78ae9105856ef04706578c5bf827323627f1743
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-05-05 14:41:58 -0400

    colname corrections

--- code/io/TrackingProcessor.py
+++ code/io/TrackingProcessor.py
@@ -36,7 +36,7 @@ class TrackingProcessor:
             player_ids = list(moment_df["playerId"].dropna().unique())  # Get all unique playerIds if 'all'
         
         # Filter the DataFrame for the specified timestamp and player_ids
-        moment_df = moment_df[(moment_df["timestamp"] == timestamp) & (moment_df["playerId"].isin(player_ids))]
+        moment_df = moment_df[(moment_df["wcTime"] == timestamp) & (moment_df["playerId"].isin(player_ids))]
         
         # Drop unnecessary columns
         moment_df = moment_df.drop(["gameId", "teamAbbr", "period", "wcTime", "gcTime", "scTime", "gameDate"], axis=1)
@@ -45,7 +45,7 @@ class TrackingProcessor:
 
     def ball_position_at_moment(moment_df, timestamp):
         # Filter the DataFrame for the specified timestamp and the ball (team_id = -1)
-        ball_df = moment_df[(moment_df["timestamp"] == timestamp) & (moment_df["teamId"] == -1)]
+        ball_df = moment_df[(moment_df["wcTime"] == timestamp) & (moment_df["teamId"] == -1)]
         
         # Drop unnecessary columns
         ball_df = ball_df.drop(["gameId", "teamAbbr", "period", "wcTime", "gcTime", "scTime", "gameDate"], axis=1)
