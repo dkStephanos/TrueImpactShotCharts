4f870a8175acb6f9bef4e9852870d84b353fad8e
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-05-14 13:47:56 -0400

    small tweaks

--- code/util/StatsUtil.py
+++ code/util/StatsUtil.py
@@ -225,12 +225,16 @@ class StatsUtil:
 
         
     def _calculate_rebound_for_row(row, tracking_df, hexbin_region_data, hexbin_basket_x, shot_region_specific=False):
+        if row['made'] == True:
+            # If the shot was made, just return NA
+            return pd.NA, pd.NA
+        
         # Identify the hexbin data for the shot's classified region (if prompted)
         region_data = hexbin_region_data[hexbin_region_data['region'] == row['shot_classification']] if shot_region_specific else hexbin_region_data
 
         # Calculate the rebound chances
         _, team_rebound_chances = StatsUtil.calculate_rebound_chances(
-            tracking_df.copy().loc[tracking_df['gameId'] == row['gameId']], row['shot_time'], row["basket_x"], region_data, hexbin_basket_x
+            tracking_df.copy().loc[tracking_df['gameId'] == row['gameId']], row['shot_time'], row["basketX"], region_data, hexbin_basket_x
         )
 
         # Return rebound chances for defensive and offensive teams
