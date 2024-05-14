9aa335bd846e00a5cd9c74682f9efe5d0badefad
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-05-14 12:28:21 -0400

    improving extract_shots_and_rebounds

--- code/io/ActionProcessor.py
+++ code/io/ActionProcessor.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from code.io.PossessionProcessor import PossessionProcessor
+
 
 class ActionProcessor:
     """
     Class for identifying/extracting game actions using a combnination of event, possesssion and tracking data
     """
-    
+
     def extract_shots_and_rebounds(event_df, tracking_df):
         """
         Extracts missed shots and their corresponding rebounds, mapping the shot location to the rebound location using tracking data.
@@ -23,36 +23,82 @@ class ActionProcessor:
         tracking_df = tracking_df[tracking_df["teamId"] == "-1"].copy()
 
         # Filter for shots and rebounds
-        shots_df = event_df[event_df['eventType'] == 'SHOT'].drop(columns=['dReb']).copy()
-        rebounds_df = event_df[event_df['eventType'] == 'REB'][['gameId', 'period', 'wcTime', 'dReb']].copy()
-        
+        shots_df = (
+            event_df[event_df["eventType"] == "SHOT"].drop(columns=["dReb"]).copy()
+        )
+        rebounds_df = event_df[event_df["eventType"] == "REB"][
+            ["gameId", "teamId", "period", "wcTime", "dReb"]
+        ].copy()
+
         # Merging shots and rebounds on 'gameId' and 'period', then filtering and deduplicating
-        merged_df = pd.merge(rebounds_df, shots_df, on=['gameId', 'period'], suffixes=('_reb', '_shot'))
-        valid_pairs = merged_df[merged_df['wcTime_shot'] < merged_df['wcTime_reb']]
-        valid_pairs = valid_pairs.sort_values(by=['gameId', 'period', 'wcTime_reb', 'wcTime_shot'], ascending=[True, True, True, False])
-        valid_pairs = valid_pairs.drop_duplicates(subset=['gameId', 'period', 'wcTime_shot'])
-        valid_pairs = valid_pairs.drop_duplicates(subset=['gameId', 'period', 'wcTime_reb'])
+        merged_df = pd.merge(
+            rebounds_df, shots_df, on=["gameId", "period"], suffixes=("_reb", "_shot")
+        )
+        valid_pairs = merged_df[merged_df["wcTime_shot"] < merged_df["wcTime_reb"]]
+        valid_pairs = valid_pairs.sort_values(
+            by=["gameId", "period", "wcTime_reb", "wcTime_shot"],
+            ascending=[True, True, True, False],
+        )
+        valid_pairs = valid_pairs.drop_duplicates(
+            subset=["gameId", "period", "wcTime_shot"]
+        )
+        valid_pairs = valid_pairs.drop_duplicates(
+            subset=["gameId", "period", "wcTime_reb"]
+        )
 
         # Merge with tracking data for positions
-        valid_pairs = pd.merge(valid_pairs, tracking_df[['gameId', 'wcTime', 'x', 'y']], left_on=['gameId', 'wcTime_shot'], right_on=['gameId', 'wcTime'], how='left', suffixes=('', '_shot'))
-        valid_pairs = valid_pairs.rename(columns={'x': 'shot_x', 'y': 'shot_y'})
-        valid_pairs = valid_pairs.drop(columns=['wcTime'])
+        valid_pairs = pd.merge(
+            valid_pairs,
+            tracking_df[["gameId", "wcTime", "x", "y"]],
+            left_on=["gameId", "wcTime_shot"],
+            right_on=["gameId", "wcTime"],
+            how="left",
+            suffixes=("", "_shot"),
+        )
+        valid_pairs = valid_pairs.rename(columns={"x": "shot_x", "y": "shot_y"})
+        valid_pairs = valid_pairs.drop(columns=["wcTime"])
 
-        valid_pairs = pd.merge(valid_pairs, tracking_df[['gameId', 'wcTime', 'x', 'y']], left_on=['gameId', 'wcTime_reb'], right_on=['gameId', 'wcTime'], how='left', suffixes=('_shot', '_reb'))
-        valid_pairs = valid_pairs.rename(columns={'x': 'rebound_x', 'y': 'rebound_y'})
-        valid_pairs = valid_pairs.drop(columns=['wcTime'])
+        valid_pairs = pd.merge(
+            valid_pairs,
+            tracking_df[["gameId", "wcTime", "x", "y"]],
+            left_on=["gameId", "wcTime_reb"],
+            right_on=["gameId", "wcTime"],
+            how="left",
+            suffixes=("_shot", "_reb"),
+        )
+        valid_pairs = valid_pairs.rename(columns={"x": "rebound_x", "y": "rebound_y"})
+        valid_pairs = valid_pairs.drop(columns=["wcTime"])
 
         # Prepare the final DataFrame
-        result_df = valid_pairs.rename(columns={'wcTime_shot': 'shot_time', 'wcTime_reb': 'rebound_time', 'teamId_reb': 'rebound_teamId'})
-        result_df = result_df.dropna(subset=['shot_x', 'shot_y', 'rebound_x', 'rebound_y'])
-        
+        result_df = valid_pairs.rename(
+            columns={
+                "wcTime_shot": "shot_time",
+                "wcTime_reb": "rebound_time",
+                "teamId_reb": "rebound_teamId",
+                "teamId_shot": "teamId",
+            }
+        )
+        result_df = result_df.dropna(
+            subset=["shot_x", "shot_y", "rebound_x", "rebound_y"]
+        )
+
         # Combine made and missed shot data, ensuring that rebound data for made shots is filled with nulls
-        made_shots = pd.merge(shots_df.loc[shots_df['made'] == True], tracking_df[['gameId', 'wcTime', 'x', 'y']], left_on=['gameId', 'wcTime'], right_on=['gameId', 'wcTime'], how='left', suffixes=('', '_shot'))
-        made_shots = made_shots.rename(columns={'x': 'shot_x', 'y': 'shot_y', 'wcTime': 'shot_time'})
+        made_shots = pd.merge(
+            shots_df.loc[shots_df["made"] == True],
+            tracking_df[["gameId", "wcTime", "x", "y"]],
+            left_on=["gameId", "wcTime"],
+            right_on=["gameId", "wcTime"],
+            how="left",
+            suffixes=("", "_shot"),
+        )
+        made_shots = made_shots.rename(
+            columns={"x": "shot_x", "y": "shot_y", "wcTime": "shot_time"}
+        )
 
-        return pd.concat([
-            made_shots,  # Made shots, already included in shots_with_pos
-            result_df  # Missed shots merged with rebound data
-        ], ignore_index=True)
-        
-
+        return pd.concat(
+            [
+                made_shots,  # Made shots, already included in shots_with_pos
+                result_df,  # Missed shots merged with rebound data
+            ],
+            ignore_index=True,
+        )
